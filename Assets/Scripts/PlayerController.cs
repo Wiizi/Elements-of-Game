00de404e923de21decabe2e9e80fd9471f592af8
Wei Wang <wei.wang18@mail.mcgill.ca>
2018-09-20 01:21:12 -0400

    agent selectable

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    List<Selectable> currentlySelected;
+    List<int> selectedIds;
+
+    Vector2 leftMouseDownXYAtClick;
+
+    int SparseRay = 5;
+
+	// Use this for initialization
+	void Start () {
+        currentlySelected = new List<Selectable>();
+        selectedIds = new List<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update selection tile
+        if (Input.GetMouseButtonDown(0))
+        {
+            leftMouseDownXYAtClick = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("LIST HAS " + currentlySelected.Count);
+            for (int i = 0; i < currentlySelected.Count; i++) {
+                currentlySelected[i].Deselect();
+            }
+ 
+            currentlySelected = new List<Selectable>();
+            selectedIds = new List<int>();
+
+            Vector2 currentLeftMouseDownXYAtClick = Input.mousePosition;
+
+            int imin = (int)Mathf.Min(leftMouseDownXYAtClick.x, currentLeftMouseDownXYAtClick.x);
+            int imax = (int)Mathf.Max(leftMouseDownXYAtClick.x, currentLeftMouseDownXYAtClick.x) + SparseRay + 1;
+
+            int jmin = (int)Mathf.Min(leftMouseDownXYAtClick.y, currentLeftMouseDownXYAtClick.y);
+            int jmax = (int)Mathf.Max(leftMouseDownXYAtClick.y, currentLeftMouseDownXYAtClick.y) + SparseRay + 1;
+
+            Debug.Log("Current ij" + imin + "," + imax + "," + jmin + "," + jmax);
+            for (int i = imin; i < imax; i += SparseRay)
+                for (int j = jmin; j < jmax; j += SparseRay)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(new Vector2(i, j));
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(ray, out hitInfo))
+                    {
+                        GameObject hitObject = hitInfo.collider.transform.gameObject;
+                        Vector3 hitPos = hitInfo.point;
+                        if (hitObject == null)
+                        {
+
+                        }
+                        else
+                        {
+                            Selectable selectable = hitObject.GetComponentInParent<Selectable>();
+                            if (selectable != null && !selectedIds.Contains(selectable.ID))
+                            {
+                                selectable.Select();
+                                currentlySelected.Add(selectable);
+                                selectedIds.Add(selectable.ID);
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+            //for (int i = 0; i < currentlySelected.Count; i++)
+            //{
+            //    currentlySelected[i].Select();
+            //}
+        }
+    }
+}
---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour {
+
+    public GameObject SelectionIndicatorPrefab;
+
+    GameObject SelectionIndicator;
+
+    public int ID;
+
+	// Use this for initialization
+	void Start () {
+        SelectionIndicator = Instantiate(SelectionIndicatorPrefab, this.transform);
+        Deselect();
+        ID = this.GetHashCode();
+	}
+
+    public void Select()
+    {
+        SelectionIndicator.SetActive(true);
+    }
+
+    public void Deselect()
+    {
+        SelectionIndicator.SetActive(false);
+    }
+}
