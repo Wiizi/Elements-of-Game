2b0276e9690d0ad421943015aff585abccae29cd
Wei Wang <wei.wang18@mail.mcgill.ca>
2018-09-25 13:10:03 -0400

    update camera zooming

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,52 +4,97 @@ using System;
 
 public class CameraController : MonoBehaviour {
 
+    [Header("Panning")]
     public float panSpeed = 5f;
     public float panBorderThickness = 15f;
-    public Vector2 panLimit;
+    public float panLimit = 20f;
 
+    [Header("Rotating")]
     public float rotateSpeed;
+    public Vector2 rotateLimit;
 
-    public float zoomSpeed;
-
-    private bool isPanning;
-    private bool isRotating;
-    private bool isZooming;
+    [Header("Zooming")]
+    public float zoomSpeed = 50f;
+    public Vector2 zoomLimit = new Vector2(10f, 75f);
 
     void Start()
     {
-        isPanning = false;
-        isRotating = false;
-        isZooming = false;
-    }
-    void Update()
-    {
-        Vector3 position = transform.position;
-        
         Debug.Log("Width = " + Screen.width);
         Debug.Log("Height = " + Screen.height);
-        Debug.Log("Mouse at " + Input.mousePosition);
+    }
 
-        if (Input.mousePosition.x >= Screen.width - panBorderThickness)
+    void Update()
+    {
+        //Vector3 panUpdatedPosition = StartPanning();
+        //transform.position = panUpdatedPosition;
+
+        //float zoomUpdatedPosition = StartZooming();
+        //Camera.main.fieldOfView = zoomUpdatedPosition;
+    }
+
+    /*
+     * Move the camera on its XZ-plane
+     */
+    Vector3 StartPanning()
+    {
+        Vector3 panPosition = transform.position;
+
+        /*
+         * Move left if (Input.mousePosition.x <= panBorderThickness)
+         * or right if (Input.mousePosition.x >= Screen.width - panBorderThickness)
+         */
+        //if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.LeftControl) && Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            position.x += panSpeed * Time.deltaTime;
+            panPosition.x += panSpeed * Time.deltaTime;
         }
-        if (Input.mousePosition.x <= panBorderThickness)
+        //if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftControl) && Input.mousePosition.x <= panBorderThickness)
         {
-            position.x -= panSpeed * Time.deltaTime;
+            panPosition.x -= panSpeed * Time.deltaTime;
         }
-        if (Input.mousePosition.y >= Screen.height - panBorderThickness)
+        /*
+         * Move forward if (Input.mousePosition.y >= Screen.height - panBorderThickness)
+         * or backward if (Input.mousePosition.y <= panBorderThickness)
+         */
+        //if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.LeftControl) && Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
-            position.z += panSpeed * Time.deltaTime;
+            panPosition.z += panSpeed * Time.deltaTime;
         }
-        if (Input.mousePosition.y <= panBorderThickness)
+        //if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.LeftControl) && Input.mousePosition.y <= panBorderThickness)
         {
-            position.z -= panSpeed * Time.deltaTime;
+            panPosition.z -= panSpeed * Time.deltaTime;
         }
 
-        position.x = Mathf.Clamp(position.x, -panLimit.x, panLimit.x);
-        position.z = Mathf.Clamp(position.z, -panLimit.y, panLimit.y);
+        panPosition.x = Mathf.Clamp(panPosition.x, -panLimit, panLimit);
+        panPosition.z = Mathf.Clamp(panPosition.z, -panLimit, panLimit);
 
-        transform.position = position;       
+        //transform.position = panPosition;
+        return panPosition;
+    }
+
+    /*
+     * Move camera along X and Y axis
+     */
+    void StartRotating()
+    {
+
+    }
+
+    /*
+     * Move camera linearly along Z axis
+     */
+    float StartZooming()
+    {
+        float scrollValue = Input.mouseScrollDelta.y;
+        float FOV = Camera.main.fieldOfView;
+
+        if (scrollValue != 0)
+        {
+            FOV -= scrollValue;
+        }
+        return Mathf.Clamp(FOV, zoomLimit.x, zoomLimit.y);
     }
 }
