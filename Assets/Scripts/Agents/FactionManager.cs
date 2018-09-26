e81d5c614e2623035af1e5400f2fe45c372bbc0f
Andrei Chubarau <andrei.chubarau@irystec.com>
2018-09-26 13:09:48 -0400

    added simple factions

---
+++ Assets/Scripts/Agents/FactionManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using ColorManagement;
+using UnityEngine;
+
+namespace Agents
+{
+    [Serializable]
+    public class Faction
+    {
+        private int uid;
+        public int Uid {
+            get { return this.uid; }
+        }
+
+        private Color factionColor;
+        public Color FactionColor
+        {
+            get { return this.factionColor; }
+        }
+        
+        public Faction(int uid)
+        {
+            this.uid = uid;
+            this.factionColor = ColorManager.NextColor();
+        }
+    }
+    
+    // faction factory singleton
+    [Serializable]
+    public class FactionManagerControl
+    {
+        private static readonly FactionManagerControl FactionManagerInstance = new FactionManagerControl();
+        
+        private readonly List<Faction> _factions;
+        
+        private FactionManagerControl()
+        {
+            this._factions = new List<Faction>();
+        }
+
+        public static Faction AddNewFaction()
+        {
+            // do faction initialization stuff here
+            Faction newFaction = new Faction(GetNumFactions());
+            
+            // register faction
+            RegisterNewFaction(newFaction);
+
+            return newFaction;
+        }
+        
+        public static Faction GetFactionWithId(int uid)
+        {
+            // get faction if it exists
+            Faction faction;
+            if (FactionManagerInstance != null && uid < GetNumFactions())
+            {
+                faction = FactionManagerInstance._factions[uid];
+            }
+            else
+            {
+                faction = null;
+            }
+            
+            return faction;
+        }
+    
+        public static int GetNumFactions()
+        {
+            return FactionManagerInstance._factions.Count;
+        }
+
+        private static void RegisterNewFaction(Faction faction)
+        {
+            // do any necessary processing on factions here; ex: faction relations, etc.
+            FactionManagerInstance._factions.Add(faction);
+        }
+
+        public static List<Faction> GetFactions()
+        {
+            return FactionManagerInstance._factions;
+        }
+    }
+}
---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ColorManagement
+{
+    // maintains an iterator list of unique colors that look good
+    // if quering too many colors, random colors will be generated instead
+    public class ColorManager
+    {
+        public static ColorManager ColorManagerInstance = new ColorManager();
+        
+        private List<Color> Colors;
+        private static int colorNum;
+
+        public static Color NewColor255(int r, int g, int b)
+        {
+            return new Color(r / 255.0f, g / 255.0f, b / 255.0f);
+        }
+        
+        public ColorManager()
+        {
+            // add some nice 'proven' colors first
+            this.Colors = new List<Color>();
+            this.Colors.Add(Color.red);
+            this.Colors.Add(Color.blue);
+            this.Colors.Add(Color.yellow);
+            this.Colors.Add(Color.cyan);
+            this.Colors.Add(Color.magenta);
+            this.Colors.Add(NewColor255(122, 244, 66)); // light green
+            this.Colors.Add(NewColor255(244, 131, 66)); // orange
+            this.Colors.Add(NewColor255(119, 49, 7)); // brown
+            this.Colors.Add(NewColor255(6, 79, 18)); // dark green
+            this.Colors.Add(NewColor255(116, 13, 219)); // violet
+            this.Colors.Add(NewColor255(219, 13, 147)); // purple
+        }
+
+        public static Color NextColor()
+        {
+            Color color;
+            if (colorNum < ColorManagerInstance.Colors.Count)
+            {
+                color = ColorManagerInstance.Colors[colorNum];
+            }
+            else
+            {
+                color = UnityEngine.Random.ColorHSV();
+            }
+            
+            colorNum++;
+            return color;
+        }
+        
+    }
+}
